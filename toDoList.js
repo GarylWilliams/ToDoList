d10bf118369425469df284cab0e3c581fba7422a
Gary Williams <williamsgaryl789@gmail.com>
2017-06-25 10:03:09 -0400

    todo list

---
+++ toDoList.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+const mustacheExpress = require('mustache-express');
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+
+var html = '<form action="/" method="post">' +
+            '<h1> Daily ToDO List</h1>' +
+            '<input type="text" name="addItem" placeholder="Add a todo..." />' +
+            '<button type="submit">Submit</button>' +
+    '</form>';
+    
+app.get('/', function (req, res) {
+      res.send(html);
+  });
+
+  app.post('/', function (req, res) {
+
+    req.checkBody("addItem", "Finished off the blank todo item, now really submit something!").notEmpty();
+    var errors = req.validationErrors();
+    if (errors) {
+
+      res.render('error', {errors: errors});
+
+    } else {
+        var addItem = req.body.addItem;
+        let newToDoItem = addItem.textContent;
+        html += '<li style="list-style-type: none;">' + '<input name="toDoItem" type="checkbox">' + '<label name="toDoItem"></label>' + addItem + '</li>';
+        res.send(html);
+      }
+  });
+
+app.listen(3000, function (){
+  console.log("Started express application!");
+});
